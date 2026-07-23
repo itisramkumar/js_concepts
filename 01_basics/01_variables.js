11db7c8a92e0a907a1967d37410908c474083e9c
R.T.RAM KUMAR <rajiniram49@gmail.com>
2026-07-23 15:32:46 +0000

    variables configure

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 41338
+let accountEmail = "rajiniram02@gmail.com"
+var accountPassword = "804512"
+accountCity = "Tamil Nadu"
+
+//accountId = 2 not allowed
+accountEmail = "ramkumar@rkgroups.com"
+accountPassword = "1102552"
+accountCity = "Haryana"
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity])
