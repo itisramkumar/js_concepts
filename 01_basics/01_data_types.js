abfa53a816e7f072f1bf1bfa036c1bcaa3e507b6
R.T.RAM KUMAR <rajiniram49@gmail.com>
2026-07-24 05:50:19 +0000

    data_types update

---
+++ 01_basics/01_data_types.js
@@ -0,0 +1,15 @@
+"use strict";// treat all Js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => object
+// undefined => undefined
+// symbol => unique
+
+console.log(3+3);
+console.log("Ram");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,10 +2,16 @@ const accountId = 41338
 let accountEmail = "rajiniram02@gmail.com"
 var accountPassword = "804512"
 accountCity = "Tamil Nadu"
+let accountState
 
 //accountId = 2 not allowed
 accountEmail = "ramkumar@rkgroups.com"
 accountPassword = "1102552"
 accountCity = "Haryana"
 console.log(accountId);
-console.table([accountEmail, accountPassword, accountCity])
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail, accountPassword, accountCity,accountState])
