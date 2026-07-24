fd64bf3a2640e8b5d2cf1713c893bc647e111ebc
R.T.RAM KUMAR <rajiniram49@gmail.com>
2026-07-24 09:56:12 +0000

    file name change & convOp

---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,87 @@
+let score = 4567
+let name = "Ram"
+let letno = "45850gasj"
+let val = undefined
+let value = null
+let happy = true
+let sad = false
+
+console.log(typeof score);
+console.log(typeof (score));
+console.log(typeof sad);
+
+
+let valInNum = Number(score)
+let ab = Number(name)
+let lcd = Number(letno)
+let ef = Number(val)
+let gf = Number(value)
+let gh = String(happy)
+let ij = Number(happy)
+let ji = Number(sad);
+
+
+console.log(typeof valInNum);
+console.log(valInNum);
+console.log(ab);
+console.log(lcd);
+console.log(ef);
+console.log(gf);
+console.log(gh);
+console.log(ij);
+console.log(ji);
+
+// The interview question was
+console.log(null == undefined);
+console.log(null === undefined)
+console.log(undefined);
+console.log(null);
+
+/* String ---> Number
+ "33" ---> 33
+ "2344asd"-----> NaN
+ "Ramk" --> NaN
+ uncommon vals ---> Number
+ undefined ---> NaN
+ nul --> 0
+ Boolean ---> Number 
+ true ---> 1
+ false --> 0
+ */
+
+let loggedIn = 1
+let loggedOut = 0
+
+let In = Boolean(loggedIn);
+let Out = Boolean(loggedOut);
+
+console.log("loggedIn :",In);
+console.log("loggedOut :",Out);
+
+let names = " "
+let er = Boolean(names);
+let namess = "ram"
+let ers = Boolean(namess);
+console.log(er)
+console.log(ers)
+/* Number ---> Boolean
+1 ---> True
+0 ---> False
+
+String ----> Boolean
+"Ram" ---> true
+"" ---> false
+" " ---> true
+*/
+
+let numbers = 45654
+let nom = String(numbers)
+console.log( typeof nom);
+
+console.log(nom);
+
+/* String ---> Number
+4522562 ---> 4522562
+
+*/
+
