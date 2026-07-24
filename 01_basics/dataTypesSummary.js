c5c73da4f322377ff8eb2aa5a90f3db8a42b1bb8
R.T.RAM KUMAR <rajiniram49@gmail.com>
2026-07-24 14:45:54 +0000

    summary on data types

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,10 @@
+// Primitive 
+
+// 7 types : String, Boolean, null, undefined, Symbol, BigInt (scientific val)
+// Symbol is used to make a value unique
+
+// Java Script is used to make type defined for every variable
+//JavaScript is a dynamically typed language, meaning that type checks are performed at runtime rather than during compilation.
+
+// Reference Type--- alias--- Non-primitive
+// Arrray, objects, Functions
